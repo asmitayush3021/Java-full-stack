b268cb7db13910804030771e7bd4246641d63bcf
Asmit Kumar Singh <asmit.kumarsingh.cse26@heritageit.edu.in>
2026-04-05 10:12:47 +0530

    first card component

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,15 +1,13 @@
-import Footer from "./Footer";
-import Header from "./Header";
-import Food from "./Food";
+import Card from "./Card";
 function App(){
     return(
-
       <>
-       <Header />
-        <Food />
-        <Food/>
-        <Footer/>
+        <Card />
+        <Card/>
+        <Card/>
+        <Card/>
       </>
+      
        
     );
 }
