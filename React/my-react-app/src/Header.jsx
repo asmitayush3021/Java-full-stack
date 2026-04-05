ae3893ff079ba7a11ce3dd9195749942d6d19790
Asmit Kumar Singh <asmit.kumarsingh.cse26@heritageit.edu.in>
2026-04-05 09:24:26 +0530

    first component

---
+++ React/my-react-app/src/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return(
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Service </li>
+                    <li><a href="#"></a>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header;
